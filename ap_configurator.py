b68cba439a9adaf3b6ea3ef6699ac7808189c9c3
Anton Slavin <slavinant@gmail.com>
2024-07-16 00:13:19 +0300

    Create menu, styling

--- ap_configurator.py
+++ ap_configurator.py
@@ -5,7 +5,10 @@ import asyncio
 from textual import log
 from textual.app import App, ComposeResult
 from textual.containers import VerticalScroll
-from textual.widgets import Button, Header, Footer, Static
+from textual.widgets import Button, Header, Footer, Static, OptionList
+from textual.widgets.option_list import Option, Separator
+from textual.reactive import reactive
+from textual.message import Message
 from textual import events
 import subprocess
 import sys
@@ -15,21 +18,32 @@ import sys
 class APConfigurator(App):
     """Access point configuration app"""
 
+    CSS_PATH = "style.tcss"
     BINDINGS = [("q", "quit", "Quit")]
 
     def compose(self) -> ComposeResult:
         yield Header()
-        yield Footer()
         with VerticalScroll():
             yield Static("""
-            # Welcome to AutoConfig
+            # Welcome to APConfigurator
 
             This application will help you automatically configure
             your Access Point and network settings.
-
-            Press 'q' to quit or any other key to continue...
             """)
             yield Static("", id="detected-chip")
+            yield Static("", id="selected-option")
+            yield OptionList(
+                Option("Configure Access Point", id="cap"),
+                Option("Configure OpenWRT Router", id="cor"),
+                Separator(),
+                Option("View Connected Clients", id="vcc"),
+                Option("View AP Settings", id="vas"),
+                Option("View Wi-Fi Chip Information", id="vci"),
+                Separator(),
+                Option("Quit", id="vq"),
+                id="menu"
+            )
+        yield Footer()
         
 
     def on_mount(self) -> None:
@@ -47,6 +61,29 @@ class APConfigurator(App):
             return 'Realtek'
 
         return ''
+
+
+    async def on_option_list_option_selected(self, message: OptionList.OptionSelected) -> None:
+        await self.handle_option(message.option.id)
+
+
+    def show_message(self, message: str) -> None:
+        self.query_one("#selected-option", Static).update('\t' + message)
+
+
+    async def handle_option(self, option_id: str) -> None:
+        if option_id == "cap":
+            self.show_message("You selected Option 1")
+        elif option_id == "cor":
+            self.show_message("You selected Option 2")
+        elif option_id == "vcc":
+            self.show_message("You selected Option 3")
+        elif option_id == "vas":
+            self.show_message("You selected Option 4")
+        elif option_id == "vci":
+            self.show_message("You selected Option 5")
+        elif option_id == "vq":
+            self.action_quit()
 
 
     async def update_detected_chip(self) -> None:
@@ -67,10 +104,18 @@ class APConfigurator(App):
         self.query_one("#detected-chip", Static).update(result)
 
 
+    async def check_connected_clients(self) -> None:
+        pass
+
+
+    def activate_brcm_minimal(self) -> None:
+        pass
+
+
     def action_quit(self) -> None:
         self.exit()
 
 
 if __name__ == "__main__":
-    app = APConfigurator()
+    app = APConfigurator()  
     app.run()
